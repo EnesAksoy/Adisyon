47834d9c05b5d9f7b2aef9ead717a9d0e45a3706
EnesAksoy <enesaksoy1168@gmail.com>
2019-08-01 13:56:10 +0300

    first commit

---
+++ Adisyon/TablesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TablesCollectionViewCell.swift
+//  Adisyon
+//
+//  Created by ENES AKSOY on 1.08.2019.
+//  Copyright © 2019 ENES AKSOY. All rights reserved.
+//
+
+import UIKit
+
+class TablesCollectionViewCell: UICollectionViewCell {
+    
+}
